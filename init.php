ef0477c5bd5ae36129a2944f773dedf989504fdb
boris_t <boris@talovikov.ru>
2014-10-07 16:36:51 +0600

    code review; code documentation;

--- init.php
+++ init.php
@@ -1,36 +1,72 @@
 <?php
 
+/**
+ * $Id$
+ *
+ * @category  Init
+ * @package   Inc_CC
+ * @author    boris_t <boris@talovikov.ru>
+ * @copyright 2014 (c)
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
 
 $modInfo['inc_cc']['name'] = 'Include custom code';
 $modInfo['inc_cc']['abstract'] = 'Include custom code in every page (e.g. <a href="http://www.google.com/analytics/">google analytics</a>, <a href="http://metrika.yandex.ru/">yandex metrics</a>)';
 $modInfo['inc_cc']['about'] = 'by <a href="https://github.com/6RUN0">boris_t</a><br /><a href="https://github.com/6RUN0/inc_cc">Get ' . $modInfo['inc_cc']['name'] . '</a>';
 
-class incCC {
+/**
+ * Provides callbacks for event::register.
+ */
+class IncCC
+{
+    public static $opt = array();
 
-  public static $opt = array();
-
-  function init() {
-    self::$opt = config::get('inc_cc_settings');
-  }
-
-  public static function add_head(&$page) {
-    if(isset(self::$opt['head']) && !empty(self::$opt['head'])) {
-      foreach(self::$opt['head'] as $head) {
-        $page->addHeader($head);
-      }
+    /**
+     * Loading options.
+     *
+     * @return none
+     */
+    function init()
+    {
+        self::$opt = config::get('inc_cc_settings');
     }
-  }
 
-  public static function add_body(&$page) {
-    if(isset(self::$opt['body']) && !empty(self::$opt['body'])) {
-      foreach(self::$opt['body'] as $body) {
-        $page->addBody($body);
-      }
+    /**
+     * Adds code in <header>.
+     *
+     * @param Page &$page object of class Page
+     *
+     * @return none
+     * @see https://github.com/evekb/evedev-kb/blob/master/common/includes/class.page.php
+     */
+    public static function add_head(&$page)
+    {
+        if (isset(self::$opt['head']) && !empty(self::$opt['head'])) {
+            foreach (self::$opt['head'] as $head) {
+                $page->addHeader($head);
+            }
+        }
     }
-  }
+
+    /**
+     * Adds code in <body>.
+     *
+     * @param Page &$page object of class Page
+     *
+     * @return none
+     * @see https://github.com/evekb/evedev-kb/blob/master/common/includes/class.page.php
+     */
+    public static function add_body(&$page)
+    {
+        if (isset(self::$opt['body']) && !empty(self::$opt['body'])) {
+            foreach (self::$opt['body'] as $body) {
+                $page->addBody($body);
+            }
+        }
+    }
 
 }
 
-incCC::init();
-event::register('page_assembleheader', 'incCC::add_head');
-event::register('page_assemblebody', 'incCC::add_body');
+IncCC::init();
+event::register('page_assembleheader', 'IncCC::add_head');
+event::register('page_assemblebody', 'IncCC::add_body');
--- settings.php
+++ settings.php
@@ -1,69 +1,105 @@
 <?php
 
-class pIncCCForm extends pageAssembly {
+/**
+ * $Id$
+ *
+ * @category  Settings
+ * @package   Inc_CC
+ * @author    boris_t <boris@talovikov.ru>
+ * @copyright 2014 (c)
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @see       https://github.com/evekb/evedev-kb/blob/master/common/includes/class.pageassembly.php
+ */
+class pIncCCForm extends pageAssembly
+{
 
-  public $page;
-  private $settings = array();
+    public $page;
+    private $_opt = array();
 
-  function __construct() {
-    parent::__construct();
-    $this->queue('start');
-    $this->queue('form');
-  }
-
-  function start() {
-
-    $this->page = new Page();
-    $this->page->setTitle('Include custom code settings');
-    $this->page->addHeader('<link rel="stylesheet" type="text/css" href="' . KB_HOST . '/mods/inc_cc/style.css" />');
-
-    if(isset($_POST['clear'])) {
-      config::set('inc_cc_settings', NULL);
+    /**
+     * Constructor methods for this classes.
+     */
+    function __construct()
+    {
+        parent::__construct();
+        $this->queue('start');
+        $this->queue('form');
     }
 
-    $this->settings = config::get('inc_cc_settings');
+    /**
+     * Preparation of the form.
+     *
+     * @return none
+     */
+    function start()
+    {
+        $this->page = new Page();
+        $this->page->setTitle('Include custom code settings');
+        $this->page->addHeader('<link rel="stylesheet" type="text/css" href="' . KB_HOST . '/mods/inc_cc/style.css" />');
 
-    if(isset($_POST['add'])) {
-      if(isset($_POST['add_options']) && !empty($_POST['add_options'])) {
-        $this->settings[$_POST['add_options']['position']][] = $_POST['add_options']['code'];
-        config::set('inc_cc_settings', $this->settings);
-      }
-    }
-
-    if(isset($_POST['rm']) || isset($_POST['set'])) {
-      if(isset($_POST['set_options']) && !empty($_POST['set_options'])) {
-        foreach($_POST['set_options'] as $position => $arr) {
-          foreach($arr as $key => $val) {
-            if(isset($_POST['set_options'][$position][$key]['check'])) {
-              unset($this->settings[$position][$key]);
-              if(isset($_POST['set'])) {
-                $this->settings[$val['position']][] = $val['code'];
-              }
-            }
-          }
+        if (isset($_POST['clear'])) {
+            config::set('inc_cc_settings', null);
         }
-      }
-      config::set('inc_cc_settings', $this->settings);
+
+        $this->_opt = config::get('inc_cc_settings');
+
+        if (isset($_POST['add'])) {
+            if (isset($_POST['add_options']) && !empty($_POST['add_options'])) {
+                $this->_opt[$_POST['add_options']['position']][] = $_POST['add_options']['code'];
+                config::set('inc_cc_settings', $this->_opt);
+            }
+        }
+
+        if (isset($_POST['rm']) || isset($_POST['set'])) {
+            if (isset($_POST['set_options']) && !empty($_POST['set_options'])) {
+                foreach ($_POST['set_options'] as $position => $arr) {
+                    foreach ($arr as $key => $val) {
+                        if (isset($_POST['set_options'][$position][$key]['check'])) {
+                            unset($this->_opt[$position][$key]);
+                            if (isset($_POST['set'])) {
+                                $this->_opt[$val['position']][] = $val['code'];
+                            }
+                        }
+                    }
+                }
+            }
+            config::set('inc_cc_settings', $this->_opt);
+        }
     }
 
-  }
+    /**
+     * Render of the form.
+     *
+     * @return string html
+     */
+    function form()
+    {
+        global $smarty;
+        $smarty->assign('inc_cc_settings', $this->_opt);
+        return $smarty->fetch(get_tpl('./mods/inc_cc/inc_cc_form'));
+    }
 
-  function form() {
-    global $smarty;
-    $smarty->assign('inc_cc_settings', $this->settings);
-    return $smarty->fetch(get_tpl('./mods/inc_cc/inc_cc_form'));
-  }
+    /**
+     * Build context.
+     *
+     * @return none
+     */
+    function context()
+    {
+        parent::__construct();
+        $this->queue('menu');
+    }
 
-  function context() {
-    parent::__construct();
-    $this->queue('menu');
-  }
-
-  function menu() {
-    require_once('common/admin/admin_menu.php');
-    return $menubox->generate();
-  }
-
+    /**
+     * Render of admin menu.
+     *
+     * @return string html
+     */
+    function menu()
+    {
+        include 'common/admin/admin_menu.php';
+        return $menubox->generate();
+    }
 }
 
 $pageAssembly = new pIncCCForm();
